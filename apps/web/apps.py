a0f77b969cdb1a85fb31fc0876dc0a5c085faff4
mollionline <79703499+mollionline@users.noreply.github.com>
2022-05-14 16:59:43 +0600

    feat: Создать организацию, подключить постгрес, добавить в админку модель (#57)
    Создала организацию, добавила организацию в админ панель, подключила постгрес, обновила зависимости, добавила файл flake игнор, почистила пустые файлы с импортами
    
    Связано:#52
    
    Co-authored-by: moldir <@auezova_moldir>

--- apps/web/apps.py
+++ apps/web/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
 
-class StoAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sto_app'
+class WebAppConfig(AppConfig):
+    name = 'apps.web'
--- core/settings.py
+++ core/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.web.apps.WebAppConfig'
 ]
 
 MIDDLEWARE = [
@@ -54,8 +53,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -73,14 +71,17 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgress',
+        'USER': 'postgress',
+        'PASSWORD': 'postgress',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
@@ -129,18 +130,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
