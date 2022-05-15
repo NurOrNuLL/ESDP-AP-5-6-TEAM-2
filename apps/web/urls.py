0442c536a050cdb1f86a0aa9ca1f5e0505302a91
asparukh01 <104005247+asparukh01@users.noreply.github.com>
2022-05-15 16:26:13 +0600

    feat: Добавить страницу списка услуг (#69)
    Добавил страницу списка услуг с поиском и сортировкой услуг
    
    Связано: #54

--- apps/web/urls.py
+++ apps/web/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views.home.views import HomePageView
-from .views.service.views import ServiceImportView
+from .views.service.views import ServiceImportView, ServiceListView
 
 service_urls = [
-    path('service/import/', ServiceImportView.as_view(), name="service_import")
+    path('service/import/', ServiceImportView.as_view(), name="service_import"),
+    path('service/list/', ServiceListView.as_view(), name="service_list")
 ]
 
 urlpatterns = [
--- apps/web/views/service/views.py
+++ apps/web/views/service/views.py
@@ -1,9 +1,12 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from urllib.parse import urlencode
+from django.db.models import Q
 from django.shortcuts import redirect, render
+from apps.web.form import SearchForm
 from ...models.service.admin import ServiceResource
-from ...forms.service.forms import ServiceImportForm
-from ...models.service.models import Service
+from apps.web.forms.service.forms import ServiceImportForm
 from tablib import Dataset
+from apps.web.models.service import Service
 
 
 class ServiceImportView(TemplateView):
@@ -33,4 +36,42 @@ class ServiceImportView(TemplateView):
                 services = Service.objects.all().delete()
                 print(services)
                 resource.import_data(dataset, dry_run=False)
+
+        return redirect('home')
+
+
+class ServiceListView(ListView):
+    template_name = 'service/service_list.html'
+    model = Service
+    context_object_name = 'services'
+    ordering = ('name', 'category', 'price_category')
+
+    def get_search_form(self):
+        return SearchForm(self.request.GET)
+
+    def get_search_value(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data.get('search')
+
+    def get(self, request, *args, **kwargs):
+        self.form = self.get_search_form()
+        self.search_value = self.get_search_value()
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = self.form
+        if self.search_value:
+            context['query'] = urlencode({'search': self.search_value})
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.search_value:
+            query = (Q(category__icontains=self.search_value)
+                     | Q(name__icontains=self.search_value)
+                     | Q(price_category__icontains=self.search_value))
+            queryset = queryset.filter(query)
+        return queryset
+
                 return redirect('home')
