628c8fdcd095549ae6a454f24234a0ede8e5af72
Imrvrs <92727678+Imrvrs@users.noreply.github.com>
2022-05-17 20:45:12 +0600

    fix: Доработал sidebar (#91)
    Связано: 85

--- infrastructure/web/static/js/sidebars.js
+++ infrastructure/web/static/js/sidebars.js
@@ -6,3 +6,28 @@
     new bootstrap.Tooltip(tooltipTriggerEl)
   })
 })()
+
+
+let sidebarBtns = Object.values($('.sidebar-btn'));
+let sidebarDivs = Object.values($('.sidebar-ul'));
+let sidebarLinks = Object.values($('.sidebar-link'));
+
+
+let path = window.location.href;
+
+
+sidebarLinks.forEach((element) => {
+  if (element.href == path) {
+    let elementParent = element.parentNode.parentNode.parentNode;
+
+    element.style.borderLeft = 'solid 3px rgb(104, 104, 255)'
+    element.style.color = 'rgb(104, 104, 255)'
+
+    sidebarDivs.forEach((element) => {
+      if (elementParent == element) {
+        sidebarBtns[sidebarDivs.indexOf(element)].classList.remove('collapsed')
+        element.classList.add('show')
+      }
+    })
+  }
+})
