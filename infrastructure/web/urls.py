e252adf72406c7ae14c43ab860ba28515cc02d99
Надежда Дроздова <104004992+avamerina@users.noreply.github.com>
2022-05-18 20:14:09 +0600

    feat: реализовала создание собственности (#105)
    Связано: #100

--- infrastructure/web/urls.py
+++ infrastructure/web/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .order.views import HomePageView
 from .service.views import ServiceImportView, ServiceListView, ServiceExportView
 from .nomenclature.views import NomenclatureCreate
+from .own.views import OwnCreate
 
 service_urls = [
     path('service/import/', ServiceImportView.as_view(), name="service_import"),
@@ -13,9 +14,14 @@ nomenclature_urls = [
     path('nomenclature/create/', NomenclatureCreate.as_view(), name="nomenclature_create")
 ]
 
+own_urls = [
+    path('contractor/<int:contrID>/own/create/', OwnCreate.as_view(), name="own_create")
+]
+
 urlpatterns = [
     path('', HomePageView.as_view(), name="home"),
 ]
 
 urlpatterns += service_urls
 urlpatterns += nomenclature_urls
+urlpatterns += own_urls
