454f355676f2b7223aef37f678ba8e9c5c05bb6c
Надежда Дроздова <104004992+avamerina@users.noreply.github.com>
2022-05-18 18:57:55 +0600

    feat(описала-модель-собственности): описала модель собственности (#103)
    Связано: #97

---
+++ models/own/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class Own(models.Model):
+    """Собственность"""
+    name = models.CharField(
+        max_length=100, null=False, blank=False,
+        verbose_name='Наименование собственности'
+    )
+    contractor = models.ForeignKey(
+        'contractor.Contractor', verbose_name='Контрагент',
+        on_delete=models.PROTECT, null=False, blank=False
+    )
+    number = models.CharField(
+        max_length=100, null=True, blank=True,
+        verbose_name='Номер'
+    )
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Собственность'
+        verbose_name_plural = 'Собственности'
