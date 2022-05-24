115a1f0ec16c020f1bc5a7c4a7d98cbb71adbfd7
Надежда Дроздова <avamerina@gmail.com>
2022-05-24 18:53:19 +0600

    feat: Реализовала удаление собственности
    Связано: #101

--- core/settings.py
+++ core/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
     'models.organization',
     'models.trade_point',
     'models.own',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
