82a869d8a17580c43ad57d1b5ff40eef6174eb20
Asparukh Meyrambekov <asparukh2001@gmail.com>
2022-05-28 12:01:17 +0600

    feat: Создать модель сотрудников
    Создал модель сотрудников
    
    Связано: #164

---
+++ models/employee/models.py
@@ -0,0 +1,61 @@
+from django.core.validators import RegexValidator
+from django.db import models
+from .validators import JSONSchemaValidator
+
+
+TRADEPOINTS_JSON_FIELD_SCHEMA = {
+    'schema': 'http://json-schema.org/draft-07/schema#',
+    'type': 'array',
+    'items': {
+        'type': 'object',
+        'properties': {
+            'id': {
+                "type": "number",
+                "minimum": 0
+            },
+        },
+        'required': ['id', ]
+    }
+}
+
+
+class Employee(models.Model):
+    """Сотрудники"""
+    MASTER = 'MASTER'
+    ADMINISTRATOR = 'ADMINISTRATOR'
+    MANAGER = 'MANAGER'
+    ROLE = [
+        (MASTER, 'MASTER'),
+        (ADMINISTRATOR, 'ADMINISTRATOR'),
+        (MANAGER, 'MANAGER'),
+    ]
+    name = models.CharField(
+        max_length=50, null=False, blank=False,
+        verbose_name='Имя'
+    )
+    surname = models.CharField(
+        max_length=50, null=False, blank=False,
+        verbose_name='Фамилия'
+    )
+    role = models.CharField(max_length=150, choices=ROLE,
+                            null=False, blank=False)
+    IIN = models.CharField(
+        null=False, blank=False, max_length=12, unique=True,
+        verbose_name='ИИН', validators=[RegexValidator(r'^\d{12,12}$')]
+    )
+    pdf = models.FileField(upload_to='pdf')
+    address = models.CharField(max_length=50, null=False, blank=False,
+        verbose_name='Адрес')
+    phone = models.CharField(
+        max_length=100, null=False, blank=False,
+        verbose_name='Телефон'
+    )
+    birthdate = models.DateField()
+    tradepoints = models.JSONField(
+        null=True, blank=True, default=list,
+        validators=[JSONSchemaValidator(limit_value=TRADEPOINTS_JSON_FIELD_SCHEMA)]
+    )
+
+    class Meta:
+        verbose_name = "Сотрудники"
+        verbose_name_plural = "Сотрудники"
