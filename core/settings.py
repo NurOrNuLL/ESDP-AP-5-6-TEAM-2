7e1d75df00eebab7cdc61adb16265596c1c1cec2
asparukh01 <104005247+asparukh01@users.noreply.github.com>
2022-05-28 16:25:46 +0600

    164 создать модель сотрудников (#177)
    * feat: Создать модель сотрудников
    
    Создал модель сотрудников
    
    Связано: #164
    
    * fix: Сделать миграции
    
    Сделал миграции
    
    Связано: #164
    
    * fix: Изменить поле id на uuid
    
    Изменил в модели пользователя и сотрудников поле id на uuid
    
    Связано: #164
    
    * fix: Изменить json схему и подключить aws s3
    
    Изменил json схему и подключил aws s3
    
    Связано: #164

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     "celery_progress",
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -161,8 +162,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "infrastructure/web/static"]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'infrastructure/web/static'),
+]
+AWS_ACCESS_KEY_ID = 'AKIA2M2OPPDRB7G7CJ7A'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'test.aspa'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_DEFAULT_ACL = 'public-read'
+AWS_LOCATION = 'static'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
+DEFAULT_FILE_STORAGE = 'core.storage_backends.MediaStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
