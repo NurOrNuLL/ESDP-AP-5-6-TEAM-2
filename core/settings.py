6e1450ee7b81cd859726994f78db3739bf9be66a
asparukh01 <104005247+asparukh01@users.noreply.github.com>
2022-05-28 12:25:51 +0600

    164 создать модель сотрудников (#169)
    * feat: Создать модель сотрудников
    
    Создал модель сотрудников
    
    Связано: #164
    
    * fix: Сделать миграции
    
    Сделал миграции
    
    Связано: #164

--- core/settings.py
+++ core/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'models.nomenclature',
     'models.organization',
     'models.trade_point',
+    'models.employee',
     'models.own',
     'rest_framework',
     'corsheaders',
