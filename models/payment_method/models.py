0a1cf0f766f4e7830712605078986ccb1f040fc5
Yerden Zhalgasbayev <zherden@gmail.com>
2022-05-30 20:42:37 +0600

    feat: Написать модель способа оплаты
    Написать модель способа оплаты. Зарегистрировал модель в админке.
    
    Связано #197

---
+++ models/payment_method/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class PaymentMethod(models.Model):
+    """Способ оплаты"""
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name='Способ оплаты')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = "Способ оплаты"
+        verbose_name_plural = "Способы оплаты"
