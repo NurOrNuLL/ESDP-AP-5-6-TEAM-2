5b2da922ecfdc97faea879901e36fea54321e6c6
rusterz <104005903+rusterz@users.noreply.github.com>
2022-05-30 22:03:40 +0600

    199 Написать модель оплаты  (#202)
    * feat: Написать модель Оплаты
    
    Написал модель Оплаты. Зарегистрировал модель в админ панели.
    
    Связано: #199
    
    * feat: Провёл миграции
    
    Создал и применил миграции
    
    Связано: #199

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG'))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 if DEBUG:
     ALLOWED_HOSTS += [os.environ.get('ALLOWED_HOST')]
 else:
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'models.employee',
     'models.own',
     'models.payment_method',
+    'models.payment',
     'rest_framework',
     'corsheaders',
     'django_filters',
---
+++ models/payment/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Payment(models.Model):
+    """Оплата"""
+    PAYMENT_STATUS_CHOICES = [('paid', 'Оплачено'), ('not_paid', 'Не оплачено')]
+    status = models.CharField(max_length=100, null=False, blank=False,
+                              choices=PAYMENT_STATUS_CHOICES, verbose_name='Статус оплаты')
+    method = models.ForeignKey(
+        'payment_method.PaymentMethod', on_delete=models.PROTECT, null=False, blank=False,
+        related_name='payments', verbose_name='Метод оплаты')
+    type = models.JSONField(null=True, blank=True, default=dict)
+
+    def __str__(self):
+        return f'{self.status, self.method, self.type}'
+
+    class Meta:
+        verbose_name = "Оплата"
+        verbose_name_plural = "Оплаты"
