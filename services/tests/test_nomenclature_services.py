24be127c621afd0fe58f414361d0847b66d40a6d
moldir <auezova.90@mail.ru>
2022-06-06 18:13:21 +0600

    feat: Написать тест на создание номенклатуры
    Написать тест на создание номенклатуры
    
    Связано:#239

--- services/tests/test_nomenclature_services.py
+++ services/tests/test_nomenclature_services.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 from models.nomenclature.models import Nomenclature
 from services.nomenclature_services import NomenclatureService
 from unittest.mock import Mock, patch
+from models.organization.models import Organization
 
 
 class NomenclatureServicesTest(TestCase):
@@ -15,3 +16,29 @@ class NomenclatureServicesTest(TestCase):
 
             self.assertEqual(returned_nomenclature, nomenclature)
             get_nomenclature.assert_called_once_with(id=nomenclature.id)
+
+    def test_create_nomenclature(self):
+        organization = Mock(spec=Organization, id=1)
+        queryset = [Mock(spec=Nomenclature)]
+        data = {
+                'name': 'СТО',
+        }
+
+        with patch('models.nomenclature.models.Nomenclature.objects.create') as create_nomenclature:
+            with patch('models.organization.models.Organization.objects.get') as get_organization:
+                create_nomenclature.return_value = Mock(
+                    spec=Nomenclature,
+                    name=data['name'],
+                    organization=organization
+                )
+
+                get_organization.return_value = organization
+
+                nomenclature = NomenclatureService.create_nomenclature(data)
+                queryset.append(nomenclature)
+                self.assertIn(nomenclature, queryset)
+                create_nomenclature.assert_called_once_with(
+                    name=data['name'],
+                    organization=organization
+                )
+                get_organization.assert_called_once_with(id=organization.id)
