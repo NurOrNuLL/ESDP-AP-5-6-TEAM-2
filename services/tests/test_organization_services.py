098b59228ada8482ecf79221d48fcdb345c1bd7c
beka <beka1990.30.10@gmail.com>
2022-06-06 18:02:28 +0600

    fix: Исправил тест на сервис организации
    Связано: #219

--- services/tests/test_organization_services.py
+++ services/tests/test_organization_services.py
@@ -1,19 +1,16 @@
-from django.test import TestCase
+from unittest import TestCase
+from unittest.mock import Mock, patch
 from models.organization.models import Organization
 from services.organization_services import OrganizationService
 
 
 class OrganizationServicesTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.organization = Organization.objects.create(
-            name='Тестовая организация',
-            address='Тестовый адрес',
-            RNN='112233445566',
-            BIN='998877445566',
-            bank_requisition='Тестовые реквизиты'
-        )
-
     def test_get_organization_by_id(self):
-        organization = OrganizationService.get_organization_by_id({"orgID": self.organization.id})
-        self.assertEqual(organization, self.organization)
+        organization = Mock(spec=Organization, id=1)
+
+        with patch('models.organization.models.Organization.objects.get') as get_organization:
+            get_organization.return_value = organization
+
+            returned_organization = OrganizationService.get_organization_by_id(kwargs={"orgID": organization.id})
+            self.assertEqual(returned_organization, organization)
+            get_organization.assert_called_once_with(id=organization.id)
