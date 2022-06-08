2773ce8ce61170f6e9c8eb6a820d422ea816377a
beka <beka1990.30.10@gmail.com>
2022-06-08 20:52:36 +0600

    fix: Удалил из проекта настроки test github workflow
    Связано: #255

--- core/settings.py
+++ core/settings.py
@@ -111,17 +111,6 @@ DATABASES = {
     }
 }
 
-if os.environ.get('GITHUB_WORKFLOW'):
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'github_actions',
-            'USER': 'postgres',
-            'PASSWORD': 'postgres',
-            'HOST': '127.0.0.1',
-            'PORT': '5432',
-        }
-    }
 
 LOGGING = {
     'version': 1,
