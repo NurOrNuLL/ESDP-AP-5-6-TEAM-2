aff6176515d5fc778f500736cfcffc1362eb9e8e
Надежда Дроздова <avamerina@gmail.com>
2022-06-09 17:21:45 +0600

    fix: Тестовый коммит для выявления ошибки запуска экшена
    Связано: #275

--- core/settings.py
+++ core/settings.py
@@ -111,17 +111,17 @@ DATABASES = {
     }
 }
 
-# if os.environ.get('GITHUB_WORKFLOW'):
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql',
-#             'NAME': 'github_actions',
-#             'USER': 'postgres',
-#             'PASSWORD': 'postgres',
-#             'HOST': '127.0.0.1',
-#             'PORT': '5432',
-#         }
-#     }
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'github_actions',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+        }
+    }
 
 
 LOGGING = {
