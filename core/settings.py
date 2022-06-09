c6f9ba0730605beff9452c6bf4c9ef5782c18b47
Надежда Дроздова <avamerina@gmail.com>
2022-06-09 19:24:07 +0600

    fix: работа
    Связано: #275

--- core/settings.py
+++ core/settings.py
@@ -193,8 +193,8 @@ if os.environ.get('GITHUB_WORKFLOW'):
         }
     }
 
-REDIS_HOST = os.environ.get('REDIS_HOST')
-REDIS_PORT = os.environ.get('REDIS_PORT')
+REDIS_HOST = 'localhost'
+REDIS_PORT = '6379'
 CELERY_BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
 CELERY_BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
 CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ":" + REDIS_PORT + '/0'
