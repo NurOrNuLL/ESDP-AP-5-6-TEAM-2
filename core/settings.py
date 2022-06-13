5c6c15f041815719ab57fb4f9dcc8b9e3a2731a8
Bekaidar <87139886+Beka757@users.noreply.github.com>
2022-06-13 19:43:25 +0600

    fix: Отредактировал настроки докер-компос (#296)
    Связано: #289

--- core/settings.py
+++ core/settings.py
@@ -60,17 +60,24 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8084',
+    'http://localhost:8085',
+    'http://localhost:8000',
+    'http://0.0.0.0:8000',
+    'http://195.201.135.12'
+]
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:8084',
+    'http://localhost:8085',
     'http://localhost:8000',
     'http://0.0.0.0:8000',
     'http://195.201.135.12'
