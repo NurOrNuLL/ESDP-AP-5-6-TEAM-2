16743cf27b45c32bd4bdd1b8b07e32e21ba3310b
mollionline <79703499+mollionline@users.noreply.github.com>
2022-06-18 16:55:04 +0600

    feat: Добавить кэширование в редис на Export exel (#359)
    Добавить кэширование в редис на Export exel
    
    Связано:#318

--- infrastructure/web/nomenclature/tasks/import_exel.py
+++ infrastructure/web/nomenclature/tasks/import_exel.py
@@ -2,12 +2,15 @@ from celery_progress.backend import ProgressRecorder
 from core import celery_app
 from .base import BaseTask
 from services.nomenclature_services import NomenclatureService
+from django.core.cache import cache
+from django.http.response import JsonResponse
 
 
-class ImporExcelTask(BaseTask):
-    name = "ImporExcelTask"
+class ImportExcelTask(BaseTask):
+    name = "ImportExcelTask"
 
-    def run(self, file, nom_id, *args, **kwargs):
+    def run(self, file: str, nom_id: int) -> JsonResponse:
+        cache.delete('nomenclatures')
         progress_recorder = ProgressRecorder(self)
         for k, file_item in enumerate(file):
             progress_recorder.set_progress(k + 1, total=len(file),
@@ -18,6 +21,6 @@ class ImporExcelTask(BaseTask):
         }
 
 
-@celery_app.task(bind=True, base=ImporExcelTask)
+@celery_app.task(bind=True, base=ImportExcelTask)
 def import_exel_file(self, *args, **kwargs):
     return super(type(self), self).run(*args, **kwargs)
