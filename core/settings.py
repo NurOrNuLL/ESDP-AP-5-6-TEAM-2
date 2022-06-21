d25e43c734e19675e2a87b070c4eea2efa8fbfc0
beka <beka1990.30.10@gmail.com>
2022-06-21 20:13:09 +0600

    fix: изменил настройки кеша для celery в settings.py

--- core/settings.py
+++ core/settings.py
@@ -224,7 +224,7 @@ CELERY_IGNORE_RESULT = True
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://redis:6379/',
+        'LOCATION': os.environ.get('ENT_REDIS_LOCATION', 'redis://127.0.0.1:6379'),
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
