793949ae7a1da4bcffa96caa3bad7f063c6e431c
Yerden Zhalgasbayev <zherden@gmail.com>
2022-06-22 21:25:48 +0600

    feat: Написал тест на оптимистичную блокировку параллельных запросов на редактирование Филиала
    Связано: #455

---
+++ infrastructure/web/trade_point/tests/test_trade_point.py
@@ -0,0 +1,38 @@
+from unittest import TestCase
+
+from models.nomenclature.models import Nomenclature
+from models.trade_point.models import TradePoint
+from services.trade_point_services import TradePointServices
+from unittest.mock import Mock, patch
+from concurrency.exceptions import RecordModifiedError
+
+
+class TradePointTest(TestCase):
+    def test_update_trade_point(self):
+        nomenclature = Mock(spec=Nomenclature, id=1)
+
+        trade_point = Mock(
+            spec=TradePoint,
+            version=0,
+            name='Филиал',
+            address='Какой-то адрес',
+            nomenclature=nomenclature
+        )
+
+        def save():
+            trade_point.version += 1
+
+        trade_point.save = save
+
+        data = {
+            'name': 'Филиал 1',
+            'address': 'Новый адрес',
+            'nomenclature': trade_point.nomenclature,
+        }
+
+        returned_trade_point_version = TradePointServices.update_trade_point(trade_point, data).version
+
+        trade_point.save()
+        with self.assertRaises(RecordModifiedError):
+            if trade_point.version != returned_trade_point_version:
+                raise RecordModifiedError(target=trade_point)
