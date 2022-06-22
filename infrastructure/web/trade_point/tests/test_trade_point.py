e7f6aefb0a05f0ed39b09781bd668999bb649207
Yerden Zhalgasbayev <zherden@gmail.com>
2022-06-22 22:09:59 +0600

    fix: Обновил тест
    Связано: #455

--- infrastructure/web/trade_point/tests/test_trade_point.py
+++ infrastructure/web/trade_point/tests/test_trade_point.py
@@ -15,23 +15,25 @@ class TradePointTest(TestCase):
                            address='Какой-то адрес', nomenclature=nomenclature
                            )
 
-        with patch('models.trade_point.models.TradePoint.save') as save_trade_point:
-            save_trade_point.return_value = None
+        with patch('models.nomenclature.models.nomenclature.objects.get') as get_nomenclature:
+            get_nomenclature.return_value = nomenclature
+            with patch('models.trade_point.models.TradePoint.save') as save_trade_point:
+                save_trade_point.return_value = None
 
-            def save():
-                trade_point.version += 1
+                def save():
+                    trade_point.version += 1
 
-            trade_point.save = save
+                trade_point.save = save
 
-            data = {
-                'name': 'Филиал 1',
-                'address': 'Новый адрес',
-                'nomenclature': trade_point.nomenclature
-            }
+                data = {
+                    'name': 'Филиал 1',
+                    'address': 'Новый адрес',
+                    'nomenclature': trade_point.nomenclature
+                }
 
-            returned_trade_point_version = TradePointServices.update_trade_point(trade_point, data).version
+                returned_trade_point_version = TradePointServices.update_trade_point(trade_point, data).version
 
-            trade_point.save()
-            with self.assertRaises(RecordModifiedError):
-                if trade_point.version != returned_trade_point_version:
-                    raise RecordModifiedError(target=trade_point)
+                trade_point.save()
+                with self.assertRaises(RecordModifiedError):
+                    if trade_point.version != returned_trade_point_version:
+                        raise RecordModifiedError(target=trade_point)
