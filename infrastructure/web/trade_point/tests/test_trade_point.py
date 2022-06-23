1217aa401ac2dbbc295623a576def31eb6678ac3
Yerden Zhalgasbayev <zherden@gmail.com>
2022-06-23 14:30:25 +0600

    fix: Обновил тест
    Связано: #455

--- infrastructure/web/trade_point/tests/test_trade_point.py
+++ infrastructure/web/trade_point/tests/test_trade_point.py
@@ -11,19 +11,19 @@ class TradePointTest(TestCase):
     def test_update_trade_point(self):
         nomenclature = Mock(spec=Nomenclature, id=1)
 
-        trade_point = Mock(spec=TradePoint, version=1, name='Филиал',
-                           address='Какой-то адрес', nomenclature=nomenclature
-                           )
-
-        with patch('models.nomenclature.models.nomenclature.objects.get') as get_nomenclature:
+        with patch('models.nomenclature.models.Nomenclature.objects.get') as get_nomenclature:
             get_nomenclature.return_value = nomenclature
             with patch('models.trade_point.models.TradePoint.save') as save_trade_point:
                 save_trade_point.return_value = None
 
+                trade_point = Mock(spec=TradePoint, version=1, name='Филиал',
+                                   address='Какой-то адрес', nomenclature=nomenclature
+                                   )
+
                 def save():
                     trade_point.version += 1
 
-                trade_point.save = save
+                trade_point.save_trade_point = save
 
                 data = {
                     'name': 'Филиал 1',
@@ -33,7 +33,7 @@ class TradePointTest(TestCase):
 
                 returned_trade_point_version = TradePointServices.update_trade_point(trade_point, data).version
 
-                trade_point.save()
+                trade_point.save_trade_point()
                 with self.assertRaises(RecordModifiedError):
                     if trade_point.version != returned_trade_point_version:
                         raise RecordModifiedError(target=trade_point)
