939226b5f654b8e636938b7f2eae43adcf4d6762
Надежда Дроздова <104004992+avamerina@users.noreply.github.com>
2022-06-24 19:02:03 +0600

    fix: Исправлено обновление изображения с учетом изменения метода загр… (#491)
    * fix: Исправлено обновление изображения с учетом изменения метода загрузки файла
    
    Связано: #451
    
    * fix: корекция по флейк8
    
    Связано: #451

--- infrastructure/web/employee/tasks.py
+++ infrastructure/web/employee/tasks.py
@@ -5,12 +5,13 @@ from services.employee_services import EmployeeServices
 
 
 @shared_task
-def upload(encoded_image, empUID):
+def upload(encoded_image, empIIN):
     decoded_image = io.BytesIO(base64.b64decode(encoded_image))
 
-    EmployeeServices.upload_image(decoded_image, empUID)
+    EmployeeServices.upload_image(decoded_image, empIIN)
 
-    employee = EmployeeServices.get_employee_by_uuid(empUID)
-    employee.image = f"https://s3.amazonaws.com/{os.environ.get('AWS_BUCKET_NAME')}/employee_image_{empUID}.png",
+    employee = EmployeeServices.get_employee_by_iin(empIIN)
+    employee.image = f"https://s3.amazonaws.com/" \
+        f"{os.environ.get('AWS_BUCKET_NAME')}/employee_image_{empIIN}.png",
     employee.save()
     return 'success'
