64bfda4b94fd6918921b425b709767ce19076623
Bekaidar <87139886+Beka757@users.noreply.github.com>
2022-06-25 17:47:51 +0600

    481 написать валидацию на ввод логина и пароля при создании аккаунта (#500)
    * fix: Написать валидацию на ввод логина и пароля при создании аккаунта
    
    Добавил валидации на длину, заглавную букву, на спец символ, на цифру
    
    Связано: #481
    
    * fix: добавил валидации на ввод логина
    
    Связано: #481

--- infrastructure/accounts/validators.py
+++ infrastructure/accounts/validators.py
@@ -13,11 +13,26 @@ def uppercase_validate(password):
         raise ValidationError("Пароль должен содержать как минимум 1 заглавную букву, A-Z.")
 
 
+def cyrillic_validate(password):
+    if re.search('[а-яА-Я]', password):
+        raise ValidationError("Пароль должен содержать только латинские буквы.")
+
+
 def symbol_validate(password):
     if not re.findall('[()[\]{}|\\`~!@#$%^&*_\-+=;:\'",<>./?]', password):
-        raise ValidationError("Пароль должен содержать не менее 1 символа: " + "()[]{}|\`~!@#$%^&*_-+=;:'\",<>./?")
+        raise ValidationError("Пароль должен содержать не менее 1 символа: " + "()[]{}|\`~!@#$%^&*_-+=;:'\",<>./?.")
 
 
 def len_validate(password):
     if len(password) < 10:
-        raise ValidationError('Пароль должен содержать не менее 12 символов')
+        raise ValidationError('Пароль должен содержать не менее 12 символов.')
+
+
+def len_username_validate(username):
+    if len(username) < 5:
+        raise ValidationError('Логин должен содержать не менее 5 символов.')
+
+
+def cyrillic_username_validate(login):
+    if re.search('[а-яА-Я]', login):
+        raise ValidationError("Логин должен содержать только латинские буквы.")
