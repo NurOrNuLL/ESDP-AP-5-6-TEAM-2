816a8330445fbe53daebb6e206ccad85d54f78bd
Bekaidar <87139886+Beka757@users.noreply.github.com>
2022-07-05 17:45:48 +0600

    fix: Убрать возможность менять статус оплаты из админки (#625)
    Убрал возможность редактирования статуса оплаты на детальных страницах заказ-наряда и и оплаты в админ панели
    
    Связано: #622

--- models/order/admin.py
+++ models/order/admin.py
@@ -8,3 +8,4 @@ class OrderAdmin(admin.ModelAdmin):
     """Заказ-наряд"""
     list_display = ('id', 'created_at', 'contractor', 'price_for_pay', 'full_price', 'status')
     list_display_links = ('id', 'created_at', 'contractor', 'price_for_pay', 'full_price', 'status')
+    readonly_fields = ('payment', )
