8284dd4f2b92b9ce67c98404c262893a1ae44269
moldir <auezova.90@mail.ru>
2022-07-07 19:13:04 +0600

    feat: Добавить возможность произвести оплату в детальную стр заказ-наряда
    Добавить возможность произвести оплату в детальную стр заказ-наряда
    
    Связано:#640

--- infrastructure/web/static/js/payment/pay_order.js
+++ infrastructure/web/static/js/payment/pay_order.js
@@ -14,17 +14,18 @@ selectElement.addEventListener('change', (event) => {
         cash.innerHTML = `
                         <input type="text" class="form-control border-secondary" id="details_cashless" name='details_cashless' hidden>
                         <label for="consignment" class="form-label">Накладное</label><br>
-                        <textarea cols=3 rows=2 type="text" class="form-control border-secondary" id="consignment" name='consignment'></textarea>
-                        <div id="the-count">
-                        <span id="current">0</span>
-                        <span id="maximum">/ 100</span>
-                        </div>
+                        <textarea cols=3 rows=2 type="text" class="form-control border-secondary" id="consignment" name='consignment' maxlength="100"></textarea>
+                            <div id="the-count">
+                                <span id="current">0</span>
+                                <span id="maximum">/ 100</span>
+                             </div>
                         <label for="invoice" class="form-label">Счет фактура</label><br>
-                        <textarea cols=3 rows=2  type="text" class="form-control border-secondary" id="invoice" name='invoice' ></textarea>
-                        <div id="the-count2">
-                        <span id="current2">0</span>
-                        <span id="maximum2">/ 100</span>
-                        </div>`
+                        <textarea cols=3 rows=2  type="text" class="form-control border-secondary" id="invoice" name='invoice' maxlength="100"></textarea>
+                            <div style="float: right; padding: 0.1rem 0 0 0; font-size: 0.875rem;" id="the-count2">
+                                <span id="current2">0</span>
+                                <span id="maximum2">/ 100</span>
+                            </div>
+                        `
         $('#consignment').keyup(function () {
 
             var characterCount = $(this).val().length,
@@ -52,19 +53,19 @@ selectElement.addEventListener('change', (event) => {
         cash.innerHTML = ''
         cash.innerHTML = `
                         <div class="form-check">
-                        <input class="form-check-input" type="radio" name="details_kaspi" id="details_kaspiQR" value="Kaspi QR">
+                        <input class="form-check-input kaspi_detail" type="radio" name="details_kaspi" id="details_kaspiQR" value="Kaspi QR">
                         <label class="form-check-label" for="details_kaspiQR">
                         Kaspi QR
                         </label>
                         </div>
                         <div class="form-check">
-                        <input class="form-check-input" type="radio" name="details_kaspi" id="details_kaspiTransfer" value="Kaspi Перевод">
+                        <input class="form-check-input kaspi_detail" type="radio" name="details_kaspi" id="details_kaspiTransfer" value="Kaspi Перевод">
                         <label class="form-check-label" for="details_kaspiTransfer">
                         Kaspi Перевод
                         </label>
                         </div>
                         <div class="form-check">
-                        <input class="form-check-input" type="radio" name="details_kaspi" id="details_kaspiRed" value="Kaspi RED">
+                        <input class="form-check-input kaspi_detail" type="radio" name="details_kaspi" id="details_kaspiRed" value="Kaspi RED">
                         <label class="form-check-label" for="details_kaspiRed">
                         Kaspi RED
                         </label>
@@ -84,7 +85,3 @@ selectElement.addEventListener('change', (event) => {
     }
 });
 
-
-// Закрытие модального окна кнопкой X и обновление страницы с актуальными данными после изменения
-
-let submit = document.querySelector('.close_modal')
