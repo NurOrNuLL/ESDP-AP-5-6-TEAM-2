aa297ebb97ec544d1f9e2a1d1ceb06d0bad32909
Danil <danilastolyarov97@gmail.com>
2022-07-07 21:20:18 +0600

    fix: Исправил url в js
    Связано: #642

--- infrastructure/web/static/js/report/report_create.js
+++ infrastructure/web/static/js/report/report_create.js
@@ -597,7 +597,7 @@ reportForm.addEventListener('submit', e => {
     e.preventDefault();
 
     if (DEBUG === true) {
-        reportSocket = new WebSocket(`ws://${window.location.host}/report/create`);
+        reportSocket = new WebSocket(`ws://${window.location.host}/report/create/`);
     }
     else {
         reportSocket = new WebSocket(`wss://${window.location.host}/report/create/`);
