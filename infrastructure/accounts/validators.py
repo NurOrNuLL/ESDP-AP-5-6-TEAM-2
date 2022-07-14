0fbcd62209cc6018435022c6493a25d55b6be34a
Imrvrs <92727678+Imrvrs@users.noreply.github.com>
2022-07-14 20:10:20 +0600

    Добавил изменение пароля в админ панель (#680)
    * fix: Исправил редактирование сотрудников
    
    Связано: #675
    
    * fix: Исправил создание сотрудника
    
    * feat: Добавил изменение пароля в админ панель

--- infrastructure/accounts/validators.py
+++ infrastructure/accounts/validators.py
@@ -3,6 +3,42 @@ import re
 from django.core.exceptions import ValidationError
 
 
+class NumberValidator(object):
+    def validate(self, password, User=None):
+        if not re.findall('\d', password):
+            raise ValidationError("Пароль должен содержать не менее 1 цифры от 0 до 9.")
+
+    def get_help_text(self):
+        return "Пароль должен содержать не менее 1 цифры от 0 до 9."
+
+
+class UppercaseValidator(object):
+    def validate(self, password, User=None):
+        if not re.findall('[A-Z]', password):
+            raise ValidationError("Пароль должен содержать как минимум 1 заглавную букву, A-Z.")
+
+    def get_help_text(self):
+        return "Пароль должен содержать как минимум 1 заглавную букву, A-Z."
+
+
+class CyrillicValidator(object):
+    def validate(self, password, User=None):
+        if re.search('[а-яА-Я]', password):
+            raise ValidationError("Пароль должен содержать только латинские буквы.")
+
+    def get_help_text(self):
+        return "Пароль должен содержать только латинские буквы."
+
+
+class SymbolValidator(object):
+    def validate(self, password, User=None):
+        if not re.findall('[()[\]{}|\\`~!@#$%^&*_\-+=;:\'",<>./?]', password):
+            raise ValidationError("Пароль должен содержать не менее 1 символа: " + "()[]{}|\`~!@#$%^&*_-+=;:'\",<>./?.")
+
+    def get_help_text(self):
+        return "Пароль должен содержать не менее 1 символа: " + "()[]{}|\`~!@#$%^&*_-+=;:'\",<>./?."
+
+
 def number_validate(password):
     if not re.findall('\d', password):
         raise ValidationError("Пароль должен содержать не менее 1 цифры от 0 до 9.")
